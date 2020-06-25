2be8a4cccb345a0f1dbe87a73c7f2d764a80e5a0
nwh20 <hnamwon@gmail.com>
2020-06-25 21:56:44 +0900

    Updates

---
+++ precache-manifest.c8327cb4a99fcc1a21fe14736c17a587.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce7689cff00c1f2ac6c33f9eb6aef26",
+    "url": "/otc-react/index.html"
+  },
+  {
+    "revision": "f0b1177b13f8645b6a22",
+    "url": "/otc-react/static/css/main.339aec23.chunk.css"
+  },
+  {
+    "revision": "7b7702a53736d984d3c8",
+    "url": "/otc-react/static/js/2.fd41e704.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/otc-react/static/js/2.fd41e704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0b1177b13f8645b6a22",
+    "url": "/otc-react/static/js/main.34b6fde9.chunk.js"
+  },
+  {
+    "revision": "9f98d03ce369ce293b76",
+    "url": "/otc-react/static/js/runtime-main.fa335277.js"
+  },
+  {
+    "revision": "33e667654149f62881dc68044b11e8f3",
+    "url": "/otc-react/static/media/about.33e66765.jpg"
+  },
+  {
+    "revision": "4ccf5c50d73b2c6c966cb051c58c5228",
+    "url": "/otc-react/static/media/grid1.4ccf5c50.png"
+  },
+  {
+    "revision": "595d83e1142c9319ef4d17c4562596e4",
+    "url": "/otc-react/static/media/grid2.595d83e1.png"
+  },
+  {
+    "revision": "488b9d43244d6ae950052cb667114ca8",
+    "url": "/otc-react/static/media/grid3.488b9d43.png"
+  },
+  {
+    "revision": "a1fc3c1b7592d840970fe23e726e63b1",
+    "url": "/otc-react/static/media/grid4.a1fc3c1b.png"
+  },
+  {
+    "revision": "b24888104aa9c4dbeda24c23a8887c1c",
+    "url": "/otc-react/static/media/main-left.b2488810.png"
+  },
+  {
+    "revision": "fe274b3edd33a112686fe89cf3f7cda5",
+    "url": "/otc-react/static/media/main-right.fe274b3e.png"
+  }
+]);
