d0612afb520beda406d14ece362d45ea95eb821b
nwh20 <hnamwon@gmail.com>
2020-06-26 11:08:23 +0900

    Updates

---
+++ precache-manifest.f7c0b05942f50d8a2877526b6cd43cbe.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ba94f30e181349df1257ffd6abfea7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8005c326dbdba2af48d8",
+    "url": "/static/css/main.339aec23.chunk.css"
+  },
+  {
+    "revision": "5120172f6492a476e870",
+    "url": "/static/js/2.3013e2bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3013e2bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8005c326dbdba2af48d8",
+    "url": "/static/js/main.92a8792c.chunk.js"
+  },
+  {
+    "revision": "2aae0a36387049204996",
+    "url": "/static/js/runtime-main.0a5377fc.js"
+  },
+  {
+    "revision": "33e667654149f62881dc68044b11e8f3",
+    "url": "/static/media/about.33e66765.jpg"
+  },
+  {
+    "revision": "4ccf5c50d73b2c6c966cb051c58c5228",
+    "url": "/static/media/grid1.4ccf5c50.png"
+  },
+  {
+    "revision": "595d83e1142c9319ef4d17c4562596e4",
+    "url": "/static/media/grid2.595d83e1.png"
+  },
+  {
+    "revision": "488b9d43244d6ae950052cb667114ca8",
+    "url": "/static/media/grid3.488b9d43.png"
+  },
+  {
+    "revision": "a1fc3c1b7592d840970fe23e726e63b1",
+    "url": "/static/media/grid4.a1fc3c1b.png"
+  },
+  {
+    "revision": "b24888104aa9c4dbeda24c23a8887c1c",
+    "url": "/static/media/main-left.b2488810.png"
+  },
+  {
+    "revision": "fe274b3edd33a112686fe89cf3f7cda5",
+    "url": "/static/media/main-right.fe274b3e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/otc-react/precache-manifest.d0574418f70f4d77cfafab08443b3bac.js"
+  "/precache-manifest.f7c0b05942f50d8a2877526b6cd43cbe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/otc-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
